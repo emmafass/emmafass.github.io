7c1137467dbaf23bc77435b7ea378f6ad19612dc
Emma <enf5cb@virginia.edu>
2016-10-07 21:46:57 -0400

    new files

--- js/controllers/FashionCtrl.js
+++ js/controllers/FashionCtrl.js
@@ -1,5 +1,11 @@
 angular.module('personalApp')
 
 .controller('FashionCtrl', function($scope, $http, $location) {
-
+	$scope.isHovering = false;
+	$scope.hoverIn = function(){
+		$scope.isHovering = true;
+	}
+	$scope.hoverOut = function(){
+		$scope.isHovering = false;
+	}
 });
